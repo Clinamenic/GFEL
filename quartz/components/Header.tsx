eccba21b463a05edec286c5fff0b046cb440e21f
exeunt <90937997+exeunt3@users.noreply.github.com>
2025-03-25 02:40:30 -0700

    Update Header.tsx

--- quartz/components/Header.tsx
+++ quartz/components/Header.tsx
@@ -26,7 +26,7 @@ const Header: QuartzComponentConstructor<Options> = (opts) => {
           <div className="mr-4 flex">
           <a href="/" className="mr-6 flex items-center space-x-2">
           <img
-          src="/logo.png"
+          src="https://github.com/Clinamenic/GFEL/blob/main/logo.png?raw=true"
           alt="Ethereum Localism"
           style={{ height: "2.5rem", objectFit: "contain" }}
             /> 
